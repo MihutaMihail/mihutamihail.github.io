69f0758e38fa54ebe1241d5eeb6c6cdb4064bc8d
Mihuta Mihail <mihutamihail95@gmail.com>
2024-01-18 10:56:10 +0100

    [UPDATE] hotels images + text

--- view/otherLocations.php
+++ view/otherLocations.php
@@ -1,35 +1,75 @@
 <div class="container">
   <div class="element">
-    <img src="assets/voyage.jpg" alt="Image 1">
-    <p>Text 1</p>
+    <img src="https://admin-voyamar.orchestra-platform.com/admin/TS/fckUserFiles/Image/AFRIQUE/TUNISIE/ORANGERS_GARDEN_VILLAS/1.jpg" alt="Image 1">
+    <div class="info">
+      <h3>Hotel A</h3>
+      <p>Number of Rooms: 2</p>
+      <p>Nights: 3</p>
+      <p>Price: $300</p>
+    </div>
   </div>
   <div class="element">
-    <img src="assets/voyage.jpg" alt="Image 2">
-    <p>Text 2</p>
+    <img src="https://www.content.fti-group.com/plain/media/3169302.jpg" alt="Image 2">
+    <div class="info">
+      <h3>Hotel A</h3>
+      <p>Number of Rooms: 2</p>
+      <p>Nights: 3</p>
+      <p>Price: $300</p>
+    </div>
   </div>
   <div class="element">
-    <img src="assets/voyage.jpg" alt="Image 3">
-    <p>Text 3</p>
+    <img src="https://admin-louvre.orchestra.paris/admin/TS/fckUserFiles/Image/Solea/MRU/photos retaillees long beach/SOLEA-LONGBEACH-006.jpg" alt="Image 3">
+    <div class="info">
+      <h3>Hotel A</h3>
+      <p>Number of Rooms: 2</p>
+      <p>Nights: 3</p>
+      <p>Price: $300</p>
+    </div>
   </div>
   <div class="element">
-    <img src="assets/voyage.jpg" alt="Image 4">
-    <p>Text 4</p>
+    <img src="https://admin-louvre.orchestra.paris/admin/TS/fckUserFiles/Image/Solea/MRU/photos retaillees pirogue/SOLEA-LAPIROGUE-003.jpg" alt="Image 4">
+    <div class="info">
+      <h3>Hotel A</h3>
+      <p>Number of Rooms: 2</p>
+      <p>Nights: 3</p>
+      <p>Price: $300</p>
+    </div>
   </div>
 
   <div class="element">
-    <img src="assets/voyage.jpg" alt="Image 5">
-    <p>Text 5</p>
+    <img src="https://back-heliades.orchestra-platform.com/admin/TS/fckUserFiles/Image/CONTENU_PRODUITS_DESTINATIONS/GRECE/CRETE/HOTELS/ANEMOS_LUXURY_GRAND_RESORT/hotel-anemos-luxury-grand-resort-07.jpg" alt="Image 5">
+    <div class="info">
+      <h3>Hotel A</h3>
+      <p>Number of Rooms: 2</p>
+      <p>Nights: 3</p>
+      <p>Price: $300</p>
+    </div>
   </div>
   <div class="element">
-    <img src="assets/voyage.jpg" alt="Image 6">
-    <p>Text 6</p>
+    <img src="https://admin-louvre.orchestra.paris/admin/TS/fckUserFiles/Image/Solea/MRU/photos retaillees sugar beach/SOLEA-SUGARBEACH-002.jpg" alt="Image 6">
+    <div class="info">
+      <h3>Hotel A</h3>
+      <p>Number of Rooms: 2</p>
+      <p>Nights: 3</p>
+      <p>Price: $300</p>
+    </div>
   </div>
   <div class="element">
-    <img src="assets/voyage.jpg" alt="Image 7">
-    <p>Text 7</p>
+    <img src="https://admin-louvre.orchestra.paris/admin/TS/fckUserFiles/Image/Solea/MRU/photos retaillees long beach/SOLEA-LONGBEACH-006.jpg" alt="Image 7">
+    <div class="info">
+      <h3>Hotel A</h3>
+      <p>Number of Rooms: 2</p>
+      <p>Nights: 3</p>
+      <p>Price: $300</p>
+    </div>
   </div>
   <div class="element">
-    <img src="assets/voyage.jpg" alt="Image 8">
-    <p>Text 8</p>
+    <img src="https://admin-voyamar.orchestra-platform.com/admin/TS/fckUserFiles/Image/ASIE/Turquie/ARNOR_DE_LUXE/1.jpg" alt="Image 8">
+    <div class="info">
+      <h3>Hotel A</h3>
+      <p>Number of Rooms: 2</p>
+      <p>Nights: 3</p>
+      <p>Price: $300</p>
+    </div>
   </div>
 </div>
